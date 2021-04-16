4baacabdef0bdabdd09e336b5f7036013f52b67c
Anderson Neves Da Silva <andersonneves_@hotmail.com>
2021-04-16 15:02:13 -0300

    Finalização de mostrar o total na tela, limpar o form após submeter e a deleção da linha da tabela após clicar

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 
-// CRIANDO A LISTA VAZIA, OBS. DEVEMOS DESCONVERTER DE TEXTO PARA ARRAY, POIS ESTAVA SALVO NO localStorage COMO TEXTO.
+/*
+Devemos destransformar de texto que foi o que fizemos com o “JSON.stringfi” para salvar no localStorage, para array novamente, e para fazer isso usamos o “JSON.parse”.
+*/
 var produtos = JSON.parse(localStorage.getItem("produtos"));
 
+// Se produtos for igual a nulo é criada uma lista vazia.
 if (produtos == null) {
   produtos = [];
 }
 
-// CHAMANDO A FUNÇÃO QUE REESCREVE A LISTA APÓS CARREGAR.
+// Chamando a função que reescreve a lista após carregar.
 reescreveLista();
 
-// FUNÇÃO QUE VALIDA OS CAMPOS DO FORM.
+// --------------------------------------------------------
+// Valida os campos do “form”.
 function trataFormulario(e) {
   e.preventDefault();
   console.log(e.target.elements);
 
-  // VALIDA CAMPOS EM BRANCO.
+  // Valida os campos em branco.
   if (e.target.elements.tipoTransacao.value == "") {
     e.target.elements.tipoTransacao.parentElement.querySelector("em").style.display = "block";
     return false;
@@ -30,7 +34,7 @@ function trataFormulario(e) {
     return false;
   }
 
-  // CRIANDO O OBJETO "novoProduto" QUE ASSOCIAMOS AOS CAMPOS DOSSO FORM USANDO O EVENTO.
+  // Criando o objeto “novoProduto” que associaremos aos campos do nosso “form” usando o evento "e".
   let novoProduto = {
     tipo: e.target.elements.tipoTransacao.value,
     nome: e.target.elements.nomeMercadoria.value,
@@ -56,13 +60,59 @@ function trataFormulario(e) {
 
   console.log(produtos);
 
+  // Limpando o rodos os campos do form buscando a referência lá no “index”.
+  document.querySelector("form").reset();
+
   return false;
+}
+
+// --------------------------------------------------------
+// Função que faz a soma do extrato.
+function somaExtrato() {
+  // Variável que guarda a soma total do extrato.
+  let total = 0;
+  
+  for (let index = 0; index < produtos.length; index++) {    
+    /*
+    Somando todos os valores, Obs. o “replace” faz a troca da “,” pelo “.” porque a função “parseFloat” não funciona para vírgula, aqui também usamos regex, a “\” é para escapar, nesse exemplo iremos substituir o “.” por nada, Obs. estou atribuindo essa linha em uma variável para não ter que ficar sempre digitando essa linha.
+    */
+    let valorQueSeraSomado = parseFloat(produtos[index].valor.replace(/\./g, "").replace(/\,/g, "."));
+    
+    console.log(valorQueSeraSomado);
+
+    /*
+    Fazer a checagem do sinal pois pois ao somar tudo quando o sinal for “-” deverá subtrair etc. e uma Obs. a forma de descobrir se um número é positivo ou negativo é só multiplicá-lo por “-1”, é  o que fazemos a seguir.
+    */
+    if (produtos[index].tipo != "1") {
+      valorQueSeraSomado = valorQueSeraSomado * -1;
+    }
+
+    total = total + valorQueSeraSomado;
+  }
+
+  return total;
+}
+
+// --------------------------------------------------------
+/* 
+Função que remove a linha ao clicar nela, Obs. por criarmos as linhas dinamicamente, estamos referenciando a nossa “tbody/ tr” aqui mesmo no JS, está no fim da linha.
+*/
+function removeLinha(e, index) {
+  // console.log(e);
+  // console.log(index);
+  // O que a função “splice” faz, ela remove elementos de um array e inseri um novo elemento no seu lugar se necessário.
+  produtos.splice(index, 1);
+  // Salvando a alteração no localStorage, Obs. lembrando que antes fazemos a transformação em texto com o "JSON.stringify".
+  localStorage.setItem("produtos", JSON.stringify(produtos));
+  // Após o uso da função “splice” chamamos a função reescreveLista.
+  reescreveLista();
 }
 
 // BUSCA O ID FORM NO INDEX E ASSOCIA O EVENTO CHAMANDO A FUNÇÃO TRATA.
 document.getElementById("meuForm").addEventListener('submit', trataFormulario);
 
 
+// --------------------------------------------------------
 // FAZ O MENU ABRIR E FECHA ELE E REFERENCIADO NOS BOTÕES QUE ESTÁ NO NAV DO  INDEX.
 function trocarMenu() {  
   /* 
@@ -77,6 +127,7 @@ function trocarMenu() {
   }
 }
 
+// --------------------------------------------------------
 // Aplicando a máscara.
 function aplicaMascara(e) {
   // Para não mostrar os números que não estão na lista dentro do "if" uso a linha de baixo.
@@ -93,14 +144,13 @@ function aplicaMascara(e) {
       // console.log("2", e.target.value, value.length, value);
     
     } else {      
-      e.target.value = value.slice(0, -2) + ',' + value.slice(value.length-2, value.length);      
       // console.log("3", e.target.value, value.length, value, value.slice(0, value.length - 2), value.slice(value.length - 2, value.length));
+      e.target.value = value.slice(0, -2) + ',' + value.slice(value.length-2, value.length);      
     }
 
     lastIndex = -1;
     value = e.target.value.replace(/^0,[0-9]+/, "").replace(/,[0-9]+$/, "").replace(/\./g, "");
     // console.log(value);
-
     if (value.length >= 4) {
       valueFinal = [];
 
@@ -122,13 +172,10 @@ function aplicaMascara(e) {
   }
 }
 
+// --------------------------------------------------------
 // CÓDIGO QUE INSERI OS VALORES, ESSA FUNÇÃO LIMPA OU APAGA TODA A LISTA ANTES DE INSERIR OS VALORES.
 function reescreveLista() {
   document.querySelector(".table-transaction-statement tbody").innerHTML = "";
-
-  // VARIÁVEL QUE GUARDA A SOMA TOTAL DO EXTRATO.
-  let total = 0;
-
   /*
   ESSE CÓDIGO SERÁ EXECUTADO PARA CADA ELEMENTO QUE ESTIVER NA LISTA, ELE CHECA SE É UMA COMPRA OU VENDA. OBS. AQUI GERA UM BUG SE LIMPARMOS O "localStorage" POIS ACHO QUE COMO "produtos" ESTÁ VAZIO APÓS LIMPAR ELE RETORNA UM ERRO POR ISSO, POIS O
   "produtos" NÃO TEM UM TOMANHO.
@@ -140,46 +187,112 @@ function reescreveLista() {
       tipoTransacao = "+";
     }
 
-    document.querySelector(".table-transaction-statement tbody").innerHTML += `
-      <tr>
+    /* 
+    Por estamos adicionando as linhas dinamicamente por aqui, minha chamada da função de remover a linha será feita aqui mesmo.
+    */
+    document.querySelector(".table-transaction-statement tbody").innerHTML += `      
+      <tr onclick="removeLinha(event, ${i})">
         <td class="text-right">` + tipoTransacao + `</td>
         <td class="">` + produtos[i].nome + `</td>
         <td class="text-right">` + produtos[i].valor + `</td>
       </tr>
     `
+  }
 
-    // SOMANDO TODOS OS VALORES, OBS. ".replace" TROCA A VÍRGULA POR PONTO PORQUE O "parseFloat" NÃO FUNCIONA PARA VÍRGULA.
-    total += parseFloat((tipoTransacao + produtos[i].valor).replace("," , "."));
+  // Se o extrato estiver vazio aparecerá uma mensagem nele, Obs. estamos colocando dinamicamente.
+  if (produtos.length == 0) {
+    document.querySelector(".table-transaction-statement tbody").innerHTML += `
+      <tr class="text-center">
+        <td colspan="3" class="text-center"> Por favor, adicione uma nova transação! </td>
+      </tr>
+    `
   }
   
-  // MOSTRANDO A SOMA DOS VALORES COM DUAS CASA DECIMAIS APÓS A VÍRGULA.
-  console.log(total.toFixed(2));
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  /*
+  Chamando a função que faz a soma total do extra e já atribuindo de forma dinâmica via “id” que está lá no meu “tfoot” do index, mostrando duas casa decimal.
+  */
+  total = somaExtrato();
+  lucroOuPrejuizo = "[ LUCRO ]";
+  // checando se o número é positivo ou negativo.
+  if (total < 0) {
+    lucroOuPrejuizo = "[ PREJUÍZO ]";
+  }
+  // chega se o valor é igual a zero.
+  if (total == 0) {
+    lucroOuPrejuizo = "";
+  }
 
   
+  totalEscrito = ("R$ " + total.toFixed(2));  
+  totalEscrito =  totalEscrito.replace("-", "");
+
+  document.getElementById("valorTotal").innerHTML = totalEscrito;
+  document.getElementById("lucroOuPrejuizo").innerHTML = lucroOuPrejuizo;
+}
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Obs. não conseguir aplicar a lógica do milhar no total para mostrar na tela, ficou dando um erro de referência na variável “valueFinalString” segui a aula assim mesmo, vídeo de explicação 3 JS em 1:20 Obs. tentei também com o toLocaleString também não deu certo (tentar depois o Fernando atribui um valor vazio no valueFinalString).
+*/
+// Transformando a variável “totalEscrito” em array, o split é para separar por “.”
+// totalEscritoArray = totalEscrito.split(".");
+
+// lastIndex = 0;
+// valueFinalString = "";
+
+// if (totalEscritoArray[0].length >= 4) {
+//   valueFinal = [];
+
+//   for (let i = totalEscritoArray[0].length; i >= 0; i--) {
+//     if ((totalEscritoArray[0].length-i) % 3 == 0 && totalEscritoArray[0].slice(i - 3, i)) {
+//       valueFinal.push(totalEscritoArray[0].slice(i - 3, i));
+//       lastIndex = i;
+//     }        
+//   }
+  
+//   valueFinalString = valueFinal.reverse().join(".");
+// }
+
+// if (totalEscritoArray[0].slice(0,lastIndex-3)) {
+//   valueFinalString = totalEscritoArray[0].slice(0,lastIndex-3) + '.' + valueFinalString;
+// }
+
+// totalEscrito = "R$ " + valueFinalString + ',' + totalEscritoArray[1];
+
+
+/*
+Máscara do Jefferson com regex
+function mascara_valor() {
+  var elemento = document.getElementById('id-valor');
+  var valor = elemento.value;
+
+  valor = valor.replace(/\D/g, '')
+  valor = valor.replace(/(\d{1})(\d{1,2})$/, "$1,$2")
+  valor = valor.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+  valor = valor.replace(/^(\d)/g, "R$ $1")
+  elemento.value = valor;
+}
+*/
